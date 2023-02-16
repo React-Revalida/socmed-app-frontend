f139fa1209c9efbda127ab5faa9cc425cee9c084
AynnnUsonn <201811226@gordoncollege.edu.ph>
2023-02-16 15:34:32 +0800

    Dependencies, Components, Pages, Service
    ~ generated some components and pages, not done
    ~ http service and auth service

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import { useState } from "react";
+import { Container, CssBaseline } from "@mui/material";
+import Feed from "./pages/Feed";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Container sx={{ marginTop: 3 }}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/feed" />}></Route>
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Actions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Actions = () => {
+  return <div>Actions (like & comment)</div>;
+};
+
+export default Actions;
