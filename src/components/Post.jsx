ef78b5a5163c9e59bcd05b94150d14944e0a0158
AynnnUsonn <201811226@gordoncollege.edu.ph>
2023-02-24 10:51:56 +0800

    likeActions, getLikesbyPost(Not done)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,13 +4,32 @@ import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect } from "react";
 import "../styles/Post.css";
 import Actions from "./Actions";
+import { useDispatch, useSelector } from "react-redux";
+import * as likeActions from "../redux/actions/likeActions";
 
 const Post = (post) => {
+  console.log(post.post);
+
+  const dispatch = useDispatch();
+  //dispatch(fetchProfile());
+
+  useEffect(() => {
+    dispatch(likeActions.fetchLikesByPost(post.post.postId));
+  }, [dispatch]);
+
+  const loading = useSelector((state) => state.like.loading);
+  const likes = useSelector((state) => state.like.likes);
+  const state = useSelector((state) => state);
+
+  // console.log(state);
+  // console.log(loading);
+  // console.log(post.post);
+  console.log(likes);
   return (
-    <div className="post">
+    <div className="post.post">
       <div className="post__avatar">
         <Avatar
         // src = {avatar}
@@ -33,13 +52,14 @@ const Post = (post) => {
             </h3>
           </div>
           <div className="post__headerDescription">
-            {/* <p>Test post desc</p> */}
+            {/* <p>Test post.post desc</p> */}
             <p>{post.post.message}</p>
           </div>
         </div>
         <img src={post.post.imageUrl} alt="" />
         <div className="post__footer">
           <ChatBubbleOutlineIcon fontSize="small" />
+          {likes.length}
           <FavoriteBorderIcon fontSize="small" />
         </div>
       </div>
