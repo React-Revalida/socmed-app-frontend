1cc32a583ad72370a8cef733b2516c63ed904740
HSAMSON@win-wks301514 <harvey.samson@eee.upd.edu.ph>
2023-02-28 11:13:54 +0800

    working follow button in who to follow

--- src/components/Widgets/FriendSuggestions/FriendSuggestionItem/FriendSuggestionItem.jsx
+++ src/components/Widgets/FriendSuggestions/FriendSuggestionItem/FriendSuggestionItem.jsx
@@ -1,8 +1,25 @@
 import Avatar from "react-avatar";
 import React from "react";
 import "./FriendSuggestionItem.css";
+import * as followActions from "../../../../redux/actions/followActions";
+import { useDispatch } from "react-redux";
 
 const FriendSuggestionItem = ({ image, displayName, username }) => {
+  const dispatch = useDispatch();
+  const [userFollowed, setUserFollowed] = React.useState(false);
+
+  const handleToggleFollow = () => {
+    if (userFollowed) {
+      dispatch(followActions.unfollowUser(username)).then(() => {
+        setUserFollowed(false);
+      });
+    } else {
+      dispatch(followActions.followUser(username)).then(() => {
+        setUserFollowed(true);
+      });
+    }
+  };
+
   return (
     <div className="friendSuggestionsItem">
       <div className="friendSuggestionImage">
@@ -16,8 +33,12 @@ const FriendSuggestionItem = ({ image, displayName, username }) => {
           <span>@{username}</span>
         </div>
       </div>
-      <div className="friendFollowButton">
-        <span>Follow</span>
+      <div
+        className="friendFollowButton"
+        style={{ cursor: "pointer" }}
+        onClick={handleToggleFollow}
+      >
+        <span>{userFollowed ? "Following" : "Follow"}</span>
       </div>
     </div>
   );
