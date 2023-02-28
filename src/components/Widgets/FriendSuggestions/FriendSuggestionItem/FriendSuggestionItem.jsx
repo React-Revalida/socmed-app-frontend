2ead42ef1c97c393e1dca0e9860d1938e0416765
HSAMSON@win-wks301514 <harvey.samson@eee.upd.edu.ph>
2023-02-28 11:22:29 +0800

    added navigate in who to follow

--- src/components/Widgets/FriendSuggestions/FriendSuggestionItem/FriendSuggestionItem.jsx
+++ src/components/Widgets/FriendSuggestions/FriendSuggestionItem/FriendSuggestionItem.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import "./FriendSuggestionItem.css";
 import * as followActions from "../../../../redux/actions/followActions";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
 
 const FriendSuggestionItem = ({ image, displayName, username }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [userFollowed, setUserFollowed] = React.useState(false);
 
   const handleToggleFollow = () => {
@@ -22,11 +24,17 @@ const FriendSuggestionItem = ({ image, displayName, username }) => {
 
   return (
     <div className="friendSuggestionsItem">
-      <div className="friendSuggestionImage">
+      <div
+        className="friendSuggestionImage"
+        onClick={() => navigate(`/profile/${username}`)}
+      >
         <Avatar src={image} name={displayName} round={true} size={50} />
       </div>
       <div className="profileCardNameCol">
-        <div className="profileCardNameColName">
+        <div
+          className="profileCardNameColName"
+          onClick={() => navigate(`/profile/${username}`)}
+        >
           <span>{displayName}</span>
         </div>
         <div className="profileCardNameColuserName">
